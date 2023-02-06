f683a2cc9edd261de4f26b6eb644875348d10cea
EmanMohamed223 <emanm7260@gmail.com>
2023-02-06 11:30:49 +0200

    Initial Commit

---
+++ HelloSwiftUiwithnocore/HelloSwiftUiwithnocoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUiwithnocoreApp.swift
+//  HelloSwiftUiwithnocore
+//
+//  Created by Eman on 06/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUiwithnocoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
